31117f08a3776e242b48fbba699b246037a8f0c3
AYogore <58088654+AYogore@users.noreply.github.com>
2021-09-05 13:30:00 -0500

    Postmand and DB connections done and working

---
+++ BankAPI/src/main/java/Models/Driver.java
@@ -0,0 +1,72 @@
+package Models;
+import io.javalin.Javalin;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		//SQL Database Connection
+		Connection con = ConnectionFactory.getConnection();//"jdbc:mariadb://database-1.ct4rayfqtdoi.us-east-2.rds.amazonaws.com:3306/innodb?user=admin&password=Csamoan75");
+		//"jdbc:mariadb://<RDS ENDPOINT FROM AWS RDS SERVICE>:<port>/<DATABASE NAME>?user=<USER NAME>&password=<PASSWORD>"
+		//notation for URI in getConnection method
+		
+		String sql = "SELECT * FROM test_table";
+		try {
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			System.out.println("==========test_table==========");
+			while(rs.next()) {
+				//invoke once since you start before the 1st and not on the 1st result
+				//first invoke advances to 1st element
+				System.out.println("string_id: ["
+						+ rs.getInt("string_id")
+						+ "] string: ["
+						+ rs.getString("string")
+						+ "]");
+				
+			}
+			System.out.println("==========test_table==========");
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		//Postman connection
+		Javalin app = Javalin.create().start(7001);
+		app.get("/", ctx -> ctx.result("Hello World"));
+		app.post("/send-data", ctx -> {
+			System.out.println(ctx.body());
+			//System.out.println(eee.body());
+			});
+		
+		app.get("/hello", eee-> eee.html("Hello Javelin!"));
+		app.get("/exception",ctx -> {
+			throw new Exception("test");
+			});
+		app.get("/clients", clients -> clients.html("all clients"));
+		//app.exception(Exception.class, (e.ctx) ->{
+		//	System.out.println("exception!!!");
+		//	ctx.status(500);
+		//});
+		app.get("/runmethod", ctx -> {
+		RunMethod();
+		});
+		
+			
+		
+		//app.error(e)
+		
+		}
+	
+	public static void RunMethod()
+	{
+		System.out.println("running method");
+	}
+	
+		
+	
+}
